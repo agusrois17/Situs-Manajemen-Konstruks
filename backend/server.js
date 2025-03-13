3ac61c0b4f0d94efe151112de4c076c91f4260e3
agusrois17 <agusrois.project@gmail.com>
2025-03-13 08:58:18 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
+const express = require("express");
+const cors = require("cors");
 const documentRoutes = require("./controllers/documentController");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 app.use("/api", documentRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
